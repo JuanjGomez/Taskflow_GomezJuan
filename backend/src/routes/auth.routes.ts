22d976063e58ad58c1be18318b85b8f107db4421
juan <451586.joan23@fje.edu>
2026-06-16 21:54:59 +0200

    comenzar de nuevo

--- backend/src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from 'express';
-import { register, login } from 'express';
-
-const router = Router();
-
-router.post('/register', register);
-router.post('/login', login);
-
-export default router;
